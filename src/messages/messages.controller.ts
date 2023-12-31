f3d50854000629fcf1388a368d8edd9085270e76
denis_levchenko <tranzi90@gmail.com>
2023-12-31 23:37:19 +0300

    validate request

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+
+  }
+}
