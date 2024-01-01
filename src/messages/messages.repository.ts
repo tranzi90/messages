05293288ee57f103e330af4bf74c263dadb70785
denis_levchenko <tranzi90@gmail.com>
2024-01-01 23:21:12 +0300

    add repository, service

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,27 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const content = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(content);
+
+    return messages[id];
+  }
+
+  async findAll() {
+    const content = await readFile('messages.json', 'utf8');
+
+    return JSON.parse(content);
+  }
+
+  async create(content: string) {
+    const data = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(data);
+
+    const id = Math.floor(Math.random() * 999)
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
