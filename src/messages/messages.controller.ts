badba94c0b1cc74af35b82e2d2dcc24e6133af50
denis_levchenko <tranzi90@gmail.com>
2024-01-02 16:07:15 +0300

    finish core code

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,23 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {
+  }
   @Get()
   listMessages() {
-
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-
+    return this.messagesService.findOne(id);
   }
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,5 +1,7 @@
 import { readFile, writeFile } from 'fs/promises';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class MessagesRepository {
   async findOne(id: string) {
     const content = await readFile('messages.json', 'utf8');
